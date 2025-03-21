6cb347f5c80a85858e226540cc54223444e6a411
HAZARDSNOW <79214768+HAZARDSNOW@users.noreply.github.com>
2025-03-21 06:05:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 import os
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-BOT_TOKEN = "7920496514:AAHidrtETgIrq6uq4m8ipA5Gd_dgJPvHP-w"
+BOT_TOKEN = "" env.**
 
 # نام هوش مصنوعی
 AI_NAME = "Hidro AI"
